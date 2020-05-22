328b64490304bd3debce22587a3d5fa9f9f59d32
kcs886 <rlacndtlr7482@gmail.com>
2020-05-22 19:13:46 +0900

    200522

--- dev_jsp/src/com/mvc2/FrontMVC2.java
+++ dev_jsp/src/com/mvc2/FrontMVC2.java
@@ -20,9 +20,31 @@ public class FrontMVC2 extends HttpServlet{
 		String contextPath = req.getContextPath();
 		String command = 
 					requestURI.substring(contextPath.length()+1);
+		int end = command.lastIndexOf(".");
+		command = command.substring(0, end);
+		
 		Controller controller = null;
-		logger.info("command"+command);
+		logger.info("command :"+command);
+		
 		//insert here - 인스턴스화 and process call 해야한다.
+		
+		String[] commands = command.split("/");
+		
+		/*
+		 * for(int i=0; i<commands.length;i++) { logger.info(commands[i]); }
+		 */
+		if("member".equals(commands[0])) {
+			controller = new MemberController();
+			String memberResult = controller.process(req, res);
+			logger.info(memberResult);
+		}else if("price".equals(commands[0])) {
+			PriceController priceController = new PriceController();
+			String priceResult = priceController.process(req, res);
+			logger.info(priceResult);
+		}
+		
+		
+		controller = null;
 	}
 	
 	@Override
--- dev_jsp/src/com/mvc2/PriceController.java
+++ dev_jsp/src/com/mvc2/PriceController.java
@@ -8,12 +8,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
-public class MemberController implements Controller{
-	Logger logger = Logger.getLogger(MemberController.class);
+public class PriceController implements Controller{
+	Logger logger = Logger.getLogger(PriceController.class);
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		logger.info("process 호출 성공");
-		return "맴버관리에 대한 리턴값입니다.";
+		String reqStr = req.getParameter("");
+		
+		return "주문관리에 대한 리턴값 입니다.";
 	}
 	
 
